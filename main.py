da195d1346762923d0b737a3c0e53bb594433249
Aniya <aniyabtts09@gmail.com>
2019-05-23 14:53:53 -0500

    fixed bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:giraFFe17@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:giraFFe17@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'giraFFE17'
@@ -11,7 +11,7 @@ app.secret_key = 'giraFFE17'
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
-    pw_hash = db.Column(db.String(120))
+    password = db.Column(db.String(120))
     blog_posts = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
@@ -51,7 +51,7 @@ def register():
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-    
+    ##line 55 is trouble for writing to db
         username_db_count = User.query.filter_by(username=username).count()
         if username_db_count > 0:
             flash('yikes! "' + username + '" is already taken and password reminders are not implemented')
@@ -81,34 +81,54 @@ def add_post():
         flash("New Blog Entry")
         return redirect('/')
     else:
-        return render_template('add-post.html')
+        return render_template('new-post.html')
 
-@app.route('/', methods=['POST', 'GET'])
-def display_post():
+@app.route('/all-post', methods=['POST', 'GET'])
+def display_posts():
 
     if request.method == 'POST':
-        blog_title = request.form['title'] 
+        blog_title = request.form['title']
         blog_text = request.form['text']
-        
+        username = request.form['username'] 
+    
     else:
         if request.args.get('id'):
             dog = request.args.get('id')
             post = Blog.query.get(dog)
-            return render_template('post.html', post=post)
+            return render_template('post.html',post=post)
             
-        blog_enteries = Blog.query.all()
-        return render_template('todos.html', blog_enteries=blog_enteries)
+        posts = Blog.query.all()
+        return render_template('all-post.html', posts=posts)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def display_users():
+
+    if request.method == 'POST':
+        username = request.form['username'] 
+    
+        
+    else:
+        if request.args.get('username'):
+            dog = request.args.get('username')
+            users = Blog.query.get(dog)
+            user_posts = User.query.all()
+            return render_template('user.html', user=user, user_posts=user_posts)
+           
+    users = User.query.all()
+    return render_template('all-users.html', users=users)
 
 def logged_in_user():
     owner = User.query.filter_by(username=session['user']).first()
     return owner
 
-endpoints_without_login = ['login', 'register',]
 
-@app.before_request
-def require_login():
-    if not ('user' in session or request.endpoint in endpoints_without_login):
-        return redirect("/login")
+
+# @app.before_request
+# def require_login():
+#     endpoints_without_login = ['login', 'register']
+#     if not ('user' in session or request.endpoint in endpoints_without_login):
+#         return redirect("/login")
 
 if __name__ == '__main__':
     app.run()
